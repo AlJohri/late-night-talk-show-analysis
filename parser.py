97771783d875943a7ac1387341f8cc92c7e0730e
Al Johri <al.johri@gmail.com>
2016-05-12 00:07:58 -0400

    parse as multiple lines

--- parser.py
+++ parser.py
@@ -13,17 +13,18 @@ os.makedirs(parsed_folder, exist_ok=True)
 for raw_filename in os.listdir(raw_folder):
 
     raw_filepath = raw_folder + raw_filename
+    parsed_filepath = parsed_folder + raw_filename[:-4] + ".txt"
 
     tree = etree.parse(raw_filepath)
     root = tree.getroot()
 
     ns_mapping = {'ns':'http://www.w3.org/ns/ttml'}
     full_text = " ".join([ptext for ptext in root.xpath('//ns:tt/ns:body/ns:div/ns:p//text()', namespaces=ns_mapping)])
-    # pp(full_text.split(">>"))
-
-    parsed_filepath = parsed_folder + raw_filename[:-4] + ".txt"
 
     with open(parsed_filepath, "w") as f:
-        f.write(full_text)
+        for line in full_text.split(">> "):
+            line = line.strip()
+            f.write(line)
+            f.write("\n")
 
     print("write", parsed_filepath)
